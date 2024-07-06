ea52e6683ecd6ecf38a8e77d524490b98f531340
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-07-05 23:07:16 -0500

    working the code

--- exercisecoffee-shop/coffee-shop/src/pages/api/order/index.js
+++ exercisecoffee-shop/coffee-shop/src/pages/api/order/index.js
@@ -4,16 +4,16 @@ export default function ordering(req, res) {
     if (req.method === "GET") {
       const sendOrders = db.orders.get();
       res.status(200).json({ order: sendOrders });
-    } //else if (req.method === "POST") {
-    //   const menuId = req.body.id;
-    //   const itemInCartAlready = db.cart.getById(menuId);
-    //   if (itemInCartAlready) {
+    } else if (req.method === "POST") {
+      const menuId = req.body.id;
+       //const itemInCartAlready = db.cart.getById(menuId);
+       //if (itemInCartAlready) {
     //     db.cart.updateById(menuId, itemInCartAlready.quantity + 1);
-    //   } else {
+     } //else //{
     //       db.cart.add(req.body);
     //     }
     //     res.status(200).json({ message: "Item added to cart" });
     //   } else {
     //     res.status(404).json({ message: "We only support GET requests" });
     //   }
-    }
+     }
